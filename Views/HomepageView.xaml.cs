69cef4ba277b2fc5c69f937a1e1990b49b4ce926
Toan <tktoan10a1@gmail.com>
2025-10-29 23:01:14 +0700

    Add logo sidebar, homepage layout

---
+++ Views/HomepageView.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace JoyLeeWrite.Views
+{
+    /// <summary>
+    /// Interaction logic for CreateChapterView.xaml
+    /// </summary>
+    public partial class HomepageView : Window
+    {
+        public HomepageView()
+        {
+            InitializeComponent();
+        }
+        private bool isSidebarCollapsed = false;
+        private void ToggleSidebar_Click(object sender, RoutedEventArgs e)
+        {
+            double from = SidebarBorder.ActualWidth;
+            double to = isSidebarCollapsed ? 300 : 75;
+
+            var animation = new DoubleAnimation
+            {
+                From = from,
+                To = to,
+                Duration = new Duration(TimeSpan.FromMilliseconds(300)),
+                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseInOut }
+            };
+
+            SidebarBorder.BeginAnimation(FrameworkElement.WidthProperty, animation);
+            Logo.BeginAnimation(FrameworkElement.WidthProperty, animation);
+            // Cập nhật hiển thị các phần tử con
+            if (isSidebarCollapsed)
+            {
+                SidebarContent.Visibility = Visibility.Visible;
+                SaveTime.Visibility = Visibility.Visible;
+                BackSeries.Visibility = Visibility.Visible;
+                Logo.Visibility = Visibility.Visible;
+                Logo.Source = new BitmapImage(new Uri("H:/C#Code/JoyLeeWrite/Resources/img/logo.png"));
+
+                isSidebarCollapsed = false;
+            }
+            else
+            {
+                SidebarContent.Visibility = Visibility.Collapsed;
+                SaveTime.Visibility = Visibility.Collapsed;
+                BackSeries.Visibility = Visibility.Collapsed;
+                Logo.Width = 75;
+                Logo.Source = new BitmapImage(new Uri("H:/C#Code/JoyLeeWrite/Resources/img/header_logo.png"));
+
+                isSidebarCollapsed = true;
+            }
+        }
+    }
+}
--- Views/WriteChapterView.xaml.cs
+++ Views/WriteChapterView.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
 
 
-namespace JoyLeeWrite
+namespace JoyLeeWrite.Views
 {
-    public partial class MainWindow : Window
+    public partial class WriteChapterView : Window
     {
 
         private bool isPlaceholder = true;
         private MainViewModel _mainVM;
-        public MainWindow()
+        public WriteChapterView()
         {
             InitializeComponent();
             this.Loaded += (s, e) =>
@@ -109,35 +110,42 @@ namespace JoyLeeWrite
 
         private void ToggleSidebar_Click(object sender, RoutedEventArgs e)
         {
+            double from = SidebarBorder.ActualWidth;
+            double to = isSidebarCollapsed ? 300 : 75;
+
+            var animation = new DoubleAnimation
+            {
+                From = from,
+                To = to,
+                Duration = new Duration(TimeSpan.FromMilliseconds(300)),
+                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseInOut }
+            };
+
+            SidebarBorder.BeginAnimation(FrameworkElement.WidthProperty, animation);
+            Logo.BeginAnimation(FrameworkElement.WidthProperty, animation);
+            // Cập nhật hiển thị các phần tử con
             if (isSidebarCollapsed)
             {
-                AnimateSidebarWidth(SidebarBorder.ActualWidth, 300);
                 SidebarContent.Visibility = Visibility.Visible;
                 SaveTime.Visibility = Visibility.Visible;
-                ToggleIcon.Text = "<";
+                BackSeries.Visibility = Visibility.Visible;
+                Logo.Visibility = Visibility.Visible;
+                Logo.Source = new BitmapImage(new Uri("H:/C#Code/JoyLeeWrite/Resources/img/logo.png"));
+                
                 isSidebarCollapsed = false;
             }
             else
             {
-                AnimateSidebarWidth(SidebarBorder.ActualWidth, 75);
                 SidebarContent.Visibility = Visibility.Collapsed;
                 SaveTime.Visibility = Visibility.Collapsed;
-                ToggleIcon.Text = ">";
+                BackSeries.Visibility = Visibility.Collapsed;
+                Logo.Width = 75;
+                Logo.Source = new BitmapImage(new Uri("H:/C#Code/JoyLeeWrite/Resources/img/header_logo.png"));
+               
                 isSidebarCollapsed = true;
             }
         }
 
-        private void AnimateSidebarWidth(double from, double to)
-        {
-            DoubleAnimation animation = new DoubleAnimation
-            {
-                From = from,
-                To = to,
-                Duration = TimeSpan.FromMilliseconds(250),
-                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseInOut }
-            };
-            SidebarBorder.BeginAnimation(WidthProperty, animation);
-        }
         private void TitleTextBox_GotFocus(object sender, RoutedEventArgs e)
         {
             var box = sender as TextBox;
